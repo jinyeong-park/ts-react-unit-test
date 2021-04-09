ae02850f6b6f226163e2c379da8bf8169e16f1d7
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-09 16:07:08 -0700

    update

--- src/test/login.test.tsx
+++ src/test/login.test.tsx
@@ -1,4 +1,3 @@
-
 import { Login } from '../components/login';
 import { LoginService } from '../services/LoginService';
 //jest.mock('./services/LoginService');
@@ -10,6 +9,7 @@ import { fireEvent, waitForElement } from '@testing-library/react';
 describe('Login component tests', () => {
 
     let container: HTMLDivElement
+    // spy object to simulate login method(public) from loginservice
     const loginServiceSpy = jest.spyOn(LoginService.prototype, 'login');
     // test setup
     beforeEach(() => {
@@ -44,27 +44,36 @@ describe('Login component tests', () => {
         expect(container.querySelector("[data-test='password-input']")?.getAttribute('name'))
             .toBe('password');
     });
+    // Testing user interaction: fireEvent change, click
+    // form is submitted or not
     it('Passes credentials correctly', () => {
         const inputs = container.querySelectorAll('input');
         const loginInput = inputs[0];
         const passwordInput = inputs[1];
         const loginButton = inputs[2];
+        // testing click event
+        fireEvent.click(loginButton);
+        // testing onchange event
         fireEvent.change(loginInput, { target: { value: 'someUser' } });
         fireEvent.change(passwordInput, { target: { value: 'somePass' } });
-        fireEvent.click(loginButton);
         expect(loginServiceSpy).toBeCalledWith('someUser', 'somePass');
     });
+    // Testing async code: waitForElement, mock resolve value
     it('Renders correctly status label - invalid login', async () => {
         loginServiceSpy.mockResolvedValueOnce(false);
         const inputs = container.querySelectorAll('input');
         const loginButton = inputs[2];
         fireEvent.click(loginButton);
+
+        // since this(login response, container.querySelector('label')) is async, this happens before login button
+        // async fn -> use waitForElement fn
         const statusLabel = await waitForElement(() =>
             container.querySelector('label')
         );
         expect(statusLabel).toBeInTheDocument();
         expect(statusLabel).toHaveTextContent('Login failed');
     });
+    // Testing async code: waitForElement, mock resolve value
     it('Renders correctly status label - valid login', async () => {
         loginServiceSpy.mockResolvedValueOnce(true);
         const inputs = container.querySelectorAll('input');
